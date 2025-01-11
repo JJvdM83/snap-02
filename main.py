921d262514850c7b23171589bb671bf410874560
JJvdM83 <jacques.vandermerwe2@gmail.com>
2025-01-11 18:23:53 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,31 @@
+GameStarted = False
+Ascore = 0
+Bscore = 0
+basic.show_icon(IconNames.HAPPY)
+music.play(music.builtin_playable_sound_effect(soundExpression.hello),
+    music.PlaybackMode.UNTIL_DONE)
+basic.clear_screen()
+basic.pause(2000)
+
+def on_forever():
+    global GameStarted, Ascore, Bscore
+    GameStarted = False
+    basic.pause(randint(1000, 5000))
+    GameStarted = True
+    basic.show_icon(IconNames.HEART)
+    while GameStarted:
+        if input.pin_is_pressed(TouchPin.P1):
+            basic.show_string("A")
+            GameStarted = False
+            Ascore += 1
+        if input.pin_is_pressed(TouchPin.P2):
+            basic.show_string("B")
+            GameStarted = False
+            Bscore += 1
+    basic.pause(1000)
+    basic.show_string("A" + str(Ascore))
+    basic.pause(2000)
+    basic.show_string("B" + str(Bscore))
+    basic.pause(2000)
+    basic.clear_screen()
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,30 @@
+let GameStarted = false
+let Ascore = 0
+let Bscore = 0
+basic.showIcon(IconNames.Happy)
+music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+basic.clearScreen()
+basic.pause(2000)
+basic.forever(function () {
+    GameStarted = false
+    basic.pause(randint(1000, 5000))
+    GameStarted = true
+    basic.showIcon(IconNames.Heart)
+    while (GameStarted) {
+        if (input.pinIsPressed(TouchPin.P1)) {
+            basic.showString("A")
+            GameStarted = false
+            Ascore += 1
+        } else if (input.pinIsPressed(TouchPin.P2)) {
+            basic.showString("B")
+            GameStarted = false
+            Bscore += 1
+        }
+    }
+    basic.pause(1000)
+    basic.showString("A" + Ascore)
+    basic.pause(2000)
+    basic.showString("B" + Bscore)
+    basic.pause(2000)
+    basic.clearScreen()
+})
